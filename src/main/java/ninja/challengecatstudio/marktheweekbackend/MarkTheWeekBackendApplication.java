bf610b58b9d1c0caa494c5d41d23ffe35d243ba3
Sebastian Zumbrunn <sebastian.zumbrunn@gmail.com>
2019-03-04 21:15:05 +0100

    Init commit

---
+++ src/main/java/ninja/challengecatstudio/marktheweekbackend/MarkTheWeekBackendApplication.java
@@ -0,0 +1,13 @@
+package ninja.challengecatstudio.marktheweekbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkTheWeekBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkTheWeekBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninja/challengecatstudio/marktheweekbackend/MarkTheWeekBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package ninja.challengecatstudio.marktheweekbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarkTheWeekBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
